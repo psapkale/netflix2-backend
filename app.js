82fa892bfedd201c0c220a0d9dd1193fd5dcd196
Prem <prempravinsapkale@gmail.com>
2023-11-02 15:41:50 +0530

    setup cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import router from './routes/user.js';
 import { errorMiddleware } from './middlewares/error.js';
 import { config } from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 config({
    path: './data/config.env',
@@ -12,6 +13,13 @@ export const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+   cors({
+      origin: process.env.FRONTEND_URL,
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      credentials: true,
+   })
+);
 
 app.get('/', (req, res) => {
    res.send('Nice Working');
